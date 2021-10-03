83ad50b130ef8e4e9f851ed9178bbe976ba0d962
armin bakhshi <arminbkh0921@yahoo.com>
2021-10-03 19:27:00 +0330

    use useFormik hook for manage state and changeHandlers

--- src/Components/SignupForm/SignupForm.jsx
+++ src/Components/SignupForm/SignupForm.jsx
@@ -1,67 +1,63 @@
-import { useState } from "react";
+import { useFormik } from "formik";
 
+const initialValues = {
+  name: "",
+  email: "",
+  password: "",
+};
 const SignupForm = () => {
-  const [formValue, setFormValue] = useState({
-    name: "",
-    email: "",
-    password: "",
+  const formik = useFormik({
+    initialValues,
   });
-
-  const changeHandler = ({ target }) => {
-    setFormValue({
-      ...formValue,
-      [target.name]: target.value,
-    });
-  };
 
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log(formValue)
+    console.log(formik.values);
   };
-
-  const { name, email, password } = formValue;
 
   return (
     <form
       className={`w-full md:max-w-lg rounded-md p-5 bg-pink-400 bg-opacity-30 backdrop-blur-md`}
       onSubmit={submitHandler}
     >
-      <h1 className={`text-blue-400 text-2xl md:text-5xl mb-10`}>Signup</h1>
+      <h1 className={`text-blue-400 text-2xl md:text-5xl mb-10 tracking-wider`}>
+        Signup
+      </h1>
       <fieldset className={`flex flex-col mb-5 text-gray-300`}>
         <label className={`mb-3 font-bold`}>Name:</label>
         <input
-          className={`px-2 py-1 rounded-md placeholder-gray-500 bg-transparent border border-gray-500 outline-none`}
+          className={`px-2 py-1 placeholder-gray-500 font-medium bg-transparent border-b-2 border-blue-400 outline-none`}
           type="text"
           name="name"
           placeholder="Name..."
-          value={name}
-          onChange={changeHandler}
+          value={formik.values.name}
+          onChange={formik.handleChange}
         />
       </fieldset>
       <fieldset className={`flex flex-col mb-5 text-gray-300`}>
         <label className={`mb-3 font-bold`}>Email:</label>
         <input
-          className={`px-2 py-1 rounded-md placeholder-gray-500 bg-transparent border border-gray-500 outline-none`}
+          className={`px-2 py-1 placeholder-gray-500 font-medium bg-transparent border-b-2 border-blue-400 outline-none`}
           type="text"
           name="email"
           placeholder="Email..."
-          value={email}
-          onChange={changeHandler}
+          value={formik.values.email}
+          onChange={formik.handleChange}
         />
       </fieldset>
       <fieldset className={`flex flex-col mb-5 text-gray-300`}>
         <label className={`mb-3 font-bold`}>Password:</label>
         <input
-          className={`px-2 py-1 rounded-md placeholder-gray-500 bg-transparent border border-gray-500 outline-none`}
+          className={`px-2 py-1 placeholder-gray-500 font-medium bg-transparent border-b-2 border-blue-400 outline-none`}
           type="text"
           name="password"
           placeholder="Password..."
-          value={password}
-          onChange={changeHandler}
+          value={formik.values.password}
+          onChange={formik.handleChange}
         />
       </fieldset>
       <button
-        className={`border border-blue-400 text-blue-400 font-bold outline-none py-2 w-full rounded-md mt-7 hover:text-white hover:bg-blue-400 transition`}
+        className={`border border-blue-400 text-blue-400 font-bold tracking-wider outline-none py-2 w-full rounded-sm mt-7 hover:text-white hover:bg-blue-400 transition`}
         type="submit"
       >
         submit
