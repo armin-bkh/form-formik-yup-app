c6a7f9dc732aedce4c677d25a7f8128323f83dee
armin bakhshi <arminbkh0921@yahoo.com>
2021-10-07 23:54:04 +0330

    update ui

--- src/Components/InputForm/CheckBoxForm.jsx
+++ src/Components/InputForm/CheckBoxForm.jsx
@@ -36,7 +36,7 @@ const CheckBoxForm = ({ name, options, onChange, values, onBlur }) => {
         <div
           className={`${
             values.length > 5 && "rounded-r-md"
-          } transition-all ease-in h-full rounded-l-md w-${values.length}/6 bg-yellow-400 ${
+          } transition-all ease-in h-full rounded-l-md ${values.length ? `w-${values.length.toString()}/6` : "w-0"}  bg-blue-400 ${
             styles.labelChecked
           }`}
         ></div>
