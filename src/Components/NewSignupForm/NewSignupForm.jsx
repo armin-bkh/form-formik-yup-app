da6e2ef27a8d2eacbc7ab2881024d1bb68d80833
arminbkh0921@yahoo.com <arminbkh0921@yahoo.com>
2022-03-18 20:07:30 +0330

    disable submit button in progres time

--- src/Components/NewSignupForm/NewSignupForm.jsx
+++ src/Components/NewSignupForm/NewSignupForm.jsx
@@ -17,8 +17,9 @@ const initialValues = {
   children: [""],
 };
 
-const onSubmit = (values) => {
+const onSubmit = (values, formik) => {
   console.log(values);
+  formik.setSubmitting(false);
 };
 
 const validationSchema = Yup.object({
@@ -53,118 +54,120 @@ const NewSignupForm = () => {
       validateOnMount={true}
       enableReinitialize={true}
     >
-      <Form
-        className={`w-full md:max-w-lg rounded-md p-5 bg-pink-400 bg-opacity-30 backdrop-filter backdrop-blur-md`}
-      >
-        <h1
-          className={`text-blue-400 text-4xl font-bold md:text-6xl mb-10 ml-3 tracking-wider header`}
+      {(formik) => (
+        <Form
+          className={`w-full md:max-w-lg rounded-md p-5 bg-pink-400 bg-opacity-30 backdrop-filter backdrop-blur-md`}
         >
-          Sign up
-        </h1>
+          <h1
+            className={`text-blue-400 text-4xl font-bold md:text-6xl mb-10 ml-3 tracking-wider header`}
+          >
+            Sign up
+          </h1>
 
-        <fieldset>
-          <label htmlFor="name">name</label>
-          <Field type="text" id="name" name="name" />
-          <ErrorMessage name="name" />
-        </fieldset>
-        <fieldset>
-          <label htmlFor="email">email</label>
-          <Field type="email" id="email" name="email" />
-          <ErrorMessage name="email">
-            {(error) => <span style={{ color: "red" }}>{error}</span>}
-          </ErrorMessage>
-        </fieldset>
-        <fieldset>
-          <label htmlFor="password">password</label>
-          <Field type="password" id="password" name="password" />
-          <ErrorMessage name="password" component={ErrorMsg} />
-        </fieldset>
-        <fieldset>
-          <label htmlFor="passwordConfirmation">password confirmation</label>
-          <Field
-            type="password"
-            id="passwordConfirmation"
-            name="passwordConfirmation"
-          />
-          <ErrorMessage name="passwordConfirmation" component="span" />
-        </fieldset>
-        <fieldset>
-          <label htmlFor="comment">comment</label>
-          <Field as="textarea" id="comment" name="comment" />
-          <ErrorMessage name="comment" />
-        </fieldset>
-        <fieldset>
-          <label htmlFor="address">address</label>
-          <Field name="address">
-            {(props) => (
-              <>
-                <input type="text" id="address" {...props.field} />
-                {props.meta.error && props.meta.touched && (
-                  <span>{props.meta.error}</span>
-                )}
-              </>
-            )}
-          </Field>
-        </fieldset>
-        <fieldset>
-          <label htmlFor="facebook">facebook profile</label>
-          <Field type="text" id="facebook" name="social.facebook" />
-          <ErrorMessage name="social.facebook" component="span" />
-        </fieldset>
-        <fieldset>
-          <label htmlFor="twitter">twitter profile</label>
-          <Field type="text" id="twitter" name="social.twitter" />
-          <ErrorMessage name="social.twitter" component="span" />
-        </fieldset>
-        <fieldset>
-          <label htmlFor="firstPh">your phone number</label>
-          <Field type="text" id="firstPh" name="phoneNumber[0]" />
-          <ErrorMessage name="phoneNumber" component="span" />
-        </fieldset>
-        <fieldset>
-          <label htmlFor="secPh">dad phone number</label>
-          <Field type="text" id="secPh" name="phoneNumber[1]" />
-          <ErrorMessage name="phoneNumber" component="span" />
-        </fieldset>
-        <fieldset>
-          <label>your childrens name</label>
-          <FieldArray name="children">
-            {({
-              form: {
-                values: { children },
-              },
-              push,
-              remove,
-            }) =>
-              children.map((child, index) => (
-                <React.Fragment key={index}>
-                  <Field
-                    type="text"
-                    id={`children[${index}]`}
-                    name={`children[${index}]`}
-                  />
-                  {children.length > 1 && (
-                    <button type="button" onClick={() => remove(index)}>
-                      -
-                    </button>
+          <fieldset>
+            <label htmlFor="name">name</label>
+            <Field type="text" id="name" name="name" />
+            <ErrorMessage name="name" />
+          </fieldset>
+          <fieldset>
+            <label htmlFor="email">email</label>
+            <Field type="email" id="email" name="email" />
+            <ErrorMessage name="email">
+              {(error) => <span style={{ color: "red" }}>{error}</span>}
+            </ErrorMessage>
+          </fieldset>
+          <fieldset>
+            <label htmlFor="password">password</label>
+            <Field type="password" id="password" name="password" />
+            <ErrorMessage name="password" component={ErrorMsg} />
+          </fieldset>
+          <fieldset>
+            <label htmlFor="passwordConfirmation">password confirmation</label>
+            <Field
+              type="password"
+              id="passwordConfirmation"
+              name="passwordConfirmation"
+            />
+            <ErrorMessage name="passwordConfirmation" component="span" />
+          </fieldset>
+          <fieldset>
+            <label htmlFor="comment">comment</label>
+            <Field as="textarea" id="comment" name="comment" />
+            <ErrorMessage name="comment" />
+          </fieldset>
+          <fieldset>
+            <label htmlFor="address">address</label>
+            <Field name="address">
+              {(props) => (
+                <>
+                  <input type="text" id="address" {...props.field} />
+                  {props.meta.error && props.meta.touched && (
+                    <span>{props.meta.error}</span>
                   )}
-                  <button type="button" onClick={() => push("")}>
-                    +
-                  </button>
-                </React.Fragment>
-              ))
-            }
-          </FieldArray>
-        </fieldset>
-        <button
-          className={`header border text-sm md:text-base border-blue-400 text-blue-400 font-bold tracking-widest outline-none py-2
-         w-full rounded-sm mt-7`}
-          type="submit"
-          //   disabled={!isValid}
-        >
-          submit
-        </button>
-      </Form>
+                </>
+              )}
+            </Field>
+          </fieldset>
+          <fieldset>
+            <label htmlFor="facebook">facebook profile</label>
+            <Field type="text" id="facebook" name="social.facebook" />
+            <ErrorMessage name="social.facebook" component="span" />
+          </fieldset>
+          <fieldset>
+            <label htmlFor="twitter">twitter profile</label>
+            <Field type="text" id="twitter" name="social.twitter" />
+            <ErrorMessage name="social.twitter" component="span" />
+          </fieldset>
+          <fieldset>
+            <label htmlFor="firstPh">your phone number</label>
+            <Field type="text" id="firstPh" name="phoneNumber[0]" />
+            <ErrorMessage name="phoneNumber" component="span" />
+          </fieldset>
+          <fieldset>
+            <label htmlFor="secPh">dad phone number</label>
+            <Field type="text" id="secPh" name="phoneNumber[1]" />
+            <ErrorMessage name="phoneNumber" component="span" />
+          </fieldset>
+          <fieldset>
+            <label>your childrens name</label>
+            <FieldArray name="children">
+              {({
+                form: {
+                  values: { children },
+                },
+                push,
+                remove,
+              }) =>
+                children.map((child, index) => (
+                  <React.Fragment key={index}>
+                    <Field
+                      type="text"
+                      id={`children[${index}]`}
+                      name={`children[${index}]`}
+                    />
+                    {children.length > 1 && (
+                      <button type="button" onClick={() => remove(index)}>
+                        -
+                      </button>
+                    )}
+                    <button type="button" onClick={() => push("")}>
+                      +
+                    </button>
+                  </React.Fragment>
+                ))
+              }
+            </FieldArray>
+          </fieldset>
+          <button
+            className={`header border text-sm md:text-base border-blue-400 text-blue-400 font-bold tracking-widest outline-none py-2
+            w-full rounded-sm mt-7 disabled:opacity-50`}
+            type="submit"
+            disabled={!formik.isValid || formik.isSubmitting}
+          >
+            submit
+          </button>
+        </Form>
+      )}
     </Formik>
   );
 };
